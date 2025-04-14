7ad14907396006b14fe660b9b15372cc493bfdaf
Vinicius Santos <2007viniciussantos@gmail.com>
2025-04-14 18:43:23 -0300

    sistema "Detran" feito.

---
+++ exercícios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`a velocidade do seu carro é ${vel}Km por hora`)
+if (vel > 60) { //condição simples!!!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurância!`)
---
+++ exercícios/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasileiro'
+console.log(`Vivendo em ${país}`)
+if (país != 'Brasil') {
+    console.log ('Brasileiro')
+} else {
+    console.log ('Estranjeiro')
+}
