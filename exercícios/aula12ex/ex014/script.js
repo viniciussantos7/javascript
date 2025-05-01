32e21586e188644482cfbfd02bee725f26dc047b
Vinicius Santos <2007viniciussantos@gmail.com>
2025-05-01 12:55:07 -0300

    verificador feito

--- exercícios/aula12ex/ex014/script.js
+++ exercícios/aula12ex/ex014/script.js
@@ -3,17 +3,17 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours() 
-    msg.innerHTML = `Agora são ${hora} horas. ${bom}`
-    if (hora >= 0 && hora < 12 ) {
-        //de dia
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //Bom dia
         img.src = 'fotomanha.png'
         document.body.style.background = '#fee86d'
     } else if (hora >- 12 && hora < 18) {
-        //de tarde
+        //Boa tarde
         img.src = 'fototarde.png'
         document.body.style.background = '#d18f37'
     } else {
-        // de noite
+        //boa noite
         img.src = 'fotonoite.png'
         document.body.style.background = '#14192e'
     }
