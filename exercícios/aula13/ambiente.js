8d53190e1823ae08d074fcd4a2996e0cf7b37ae3
Vinicius Santos <2007viniciussantos@gmail.com>
2025-05-02 21:55:31 -0300

    Teste de repetições

---
+++ exercícios/aula13/ambiente.js
@@ -0,0 +1,16 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
