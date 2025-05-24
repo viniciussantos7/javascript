5992524aa12d5d77dca5a5fe77f88ebfdfcdede2
Vinicius Santos <2007viniciussantos@gmail.com>
2025-05-23 22:32:38 -0300

    Meu último exercício.

---
+++ exercícios/aula16ex/modelo/script.js
@@ -0,0 +1,60 @@
+let num = document.querySelector('input#fnum')
+let lisat = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 1000) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lisat.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if(valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo, temos <strong>${tot}</strong> números cadastrados. <p>`
+        res.innerHTML += `<p> O maior valor informado foi <strong>${maior}. </p>`
+        res.innerHTML += `<p> O menor valor informado foi <strong>${menor}. </p>`
+        res.innerHTML += `<p>Somando todos os valores, temos <strong>${soma}</strong>.</p>`
+        res.innerHTML += `<p>A média de valores digitidos é <strong>${media}</strong>.</p>`
+    }
+}
